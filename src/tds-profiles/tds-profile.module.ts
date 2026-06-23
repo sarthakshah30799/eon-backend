3019d9b904c34e660559c66de0ce02761b7c7c3a
sarthakshah30799 <sarthakshah30799@gmail.com>
2026-06-23 16:45:00 +0530

    Build fix

--- src/tds-profiles/tds-profile.module.ts
+++ src/tds-profiles/tds-profile.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TdsProfile } from './tds-profile.entity';
 import { TdsProfileController } from './tds-profile.controller';
 import { TdsProfileService } from './tds-profile.service';
+import { UserModule } from '../users/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TdsProfile])],
+  imports: [TypeOrmModule.forFeature([TdsProfile]), UserModule],
   controllers: [TdsProfileController],
   providers: [TdsProfileService],
   exports: [TdsProfileService],
